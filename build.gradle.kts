ddd5087caefdf22e6c0f903082f125770aa0c8f8
Antonio Bertucci <archano@gmail.com>
2019-07-12 09:43:08 +0200

    Add tests around FlatMap

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.3.41"
 }
@@ -7,5 +9,15 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib-jdk8"))
     implementation("io.reactivex.rxjava2:rxjava:2.2.10")
+
+    testImplementation("junit:junit:4.12")
+    testImplementation("com.google.truth:truth:1.0")
+}
+
+tasks.withType(KotlinCompile::class.java).all {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
 }
---
+++ src/test/kotlin/io/archano/playground/FlatMapTest.kt
@@ -0,0 +1,153 @@
+package io.archano.playground
+
+import com.google.common.truth.Truth.assertThat
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import org.junit.Test
+import java.util.concurrent.Executors
+
+class FlatMapTest {
+
+    @Test
+    fun standard() {
+        Observable.just(1)
+                .doOnSubscribe(printOnSubscribed)
+                .doOnNext(printOnNext)
+                .flatMap { n ->
+                    println("flatMap $n on: $threadName")
+                    Observable.just(n)
+                            .map { it * 2 }
+                            .doOnNext(printOnNext)
+                }
+                .map { n -> "$n.$n" }
+                .doOnNext(printOnNext)
+                .subscribeOn(background)
+                .observeOn(main)
+                .subscribe {
+                    println("onNext($it) on: $threadName")
+                }
+
+        Thread.sleep(1000L)
+
+        assertThat(output).isEqualTo("""
+            subscribed on: RxCachedThreadScheduler-1
+            emitted 1 on: RxCachedThreadScheduler-1
+            flatMap 1 on: RxCachedThreadScheduler-1
+            emitted 2 on: RxCachedThreadScheduler-1
+            emitted 2.2 on: RxCachedThreadScheduler-1
+            onNext(2.2) on: RxSingleScheduler-1
+            """.trimIndent())
+    }
+
+    @Test
+    fun wrong() {
+        Observable.just(1)
+                .doOnSubscribe(printOnSubscribed)
+                .doOnNext(printOnNext)
+                .observeOn(main)
+                .flatMap { n ->
+                    println("flatMap $n on: $threadName")
+                    Observable.just(n)
+                            .map { it * 2 }
+                            .doOnNext(printOnNext)
+                }
+                .map { n -> "$n.$n" }
+                .doOnNext(printOnNext)
+                .subscribeOn(background)
+                .observeOn(main)
+                .subscribe {
+                    println("onNext($it) on: $threadName")
+                }
+
+        Thread.sleep(1000L)
+
+        assertThat(output).isEqualTo("""
+            subscribed on: RxCachedThreadScheduler-1
+            emitted 1 on: RxCachedThreadScheduler-1
+            flatMap 1 on: RxSingleScheduler-1
+            emitted 2 on: RxSingleScheduler-1
+            emitted 2.2 on: RxSingleScheduler-1
+            onNext(2.2) on: RxSingleScheduler-1
+            """.trimIndent())
+    }
+
+    @Test
+    fun variation1() {
+        Observable.just(1)
+                .doOnSubscribe(printOnSubscribed)
+                .doOnNext(printOnNext)
+                .flatMap { n ->
+                    println("flatMap $n on: $threadName")
+                    Observable.just(n)
+                            .map { it * 2 }
+                            .doOnNext(printOnNext)
+                            .subscribeOn(background)
+                }
+                .map { n -> "$n.$n" }
+                .doOnNext(printOnNext)
+                .subscribeOn(background)
+                .observeOn(main)
+                .subscribe {
+                    println("onNext($it) on: $threadName")
+                }
+
+        Thread.sleep(1000L)
+
+        assertThat(output).isEqualTo("""
+            subscribed on: RxCachedThreadScheduler-1
+            emitted 1 on: RxCachedThreadScheduler-1
+            flatMap 1 on: RxCachedThreadScheduler-1
+            emitted 2 on: RxCachedThreadScheduler-2
+            emitted 2.2 on: RxCachedThreadScheduler-2
+            onNext(2.2) on: RxSingleScheduler-1
+            """.trimIndent())
+    }
+
+    @Test
+    fun variation2() {
+        Observable.just(1)
+                .doOnSubscribe(printOnSubscribed)
+                .doOnNext(printOnNext)
+                .observeOn(main)
+                .flatMap { n ->
+                    println("flatMap $n on: $threadName")
+                    Observable.just(n)
+                            .map { it * 2 }
+                            .doOnNext(printOnNext)
+                            .subscribeOn(background)
+                }
+                .map { n -> "$n.$n" }
+                .doOnNext(printOnNext)
+                .subscribeOn(background)
+                .observeOn(main)
+                .subscribe {
+                    println("onNext($it) on: $threadName")
+                }
+
+        Thread.sleep(1000L)
+
+        assertThat(output).isEqualTo("""
+            subscribed on: RxCachedThreadScheduler-1
+            emitted 1 on: RxCachedThreadScheduler-1
+            flatMap 1 on: RxSingleScheduler-1
+            emitted 2 on: RxCachedThreadScheduler-2
+            emitted 2.2 on: RxCachedThreadScheduler-2
+            onNext(2.2) on: RxSingleScheduler-1
+            """.trimIndent())
+    }
+
+    private val main get() = Schedulers.single()
+    private val background get() = Schedulers.io()
+
+    private val output: String get() = buffer.trim()
+    private var buffer = ""
+    private fun println(txt: String) {
+        buffer += txt + "\n"
+    }
+
+    private val threadName get() = Thread.currentThread().name
+    private val printOnSubscribed: (Disposable) -> Unit = { println("subscribed on: $threadName") }
+    private val printOnNext: (Any) -> Unit = { println("emitted $it on: $threadName") }
+
+}
